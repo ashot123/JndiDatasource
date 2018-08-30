15669068e66ca007b4417d23cbdabe6656f6ca7e
Ashot <ashot2005@yahoo.com>
2018-08-30 23:50:52 +0400

    Minor changes.

---
+++ src/main/java/test/DemoController.java
@@ -0,0 +1,17 @@
+package test;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Demo.do")
+public class DemoController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/demo.jsp");
+        dispatcher.forward(request, response);
+    }
+}
